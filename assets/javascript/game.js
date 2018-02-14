5a248badb5d1d5ee32eb8c6d5184ac475f87a25d
unknown <lukemyers0921@gmail.com>
2018-02-14 14:56:34 -0500

    work done at hospital

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+
+    var obi = {name:"obi-wan kenobi", attack:5, counterAttack: 10,health:120}
+    var luke ={ name:"luke skywalker", attack:10, counterAttack: 10,health:100}
+    var maul = {name:"darth sidious", attack:5, counterAttack: 10,health:150}
+    var sidious = {name:"darth mole", attack:5, counterAttack: 10,health:180}
+    var hero = {};
+    var villan = {};
+
+
